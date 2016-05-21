98588d3f1f780a342c6a2106d88d8c74e1e15447
alexbajzat <alex.bajzat@gmail.com>
2016-05-21 16:18:45 +0300

    finished login layout

--- app/src/main/java/com/gallery/sync/MainActivity.java
+++ app/src/main/java/com/gallery/sync/MainActivity.java
@@ -2,12 +2,38 @@ package com.gallery.sync;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.login_email)
+    EditText mLoginUser;
+    @Bind(R.id.login_password)
+    EditText mPassword;
+    @Bind(R.id.login_button)
+    TextView mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doLogin();
+            }
+        });
+    }
+
+    private void doLogin() {
+
+        Toast.makeText(this, "You clicked", Toast.LENGTH_LONG).show();
     }
 }
